5d79efda12244e39ce302d0417be1543c5dfcc32
Quentin <quentin@nivelais.com>
2022-10-03 19:29:45 +0200

    Adding shared repository and launching first build

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -13,16 +13,6 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (_, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 export default {
@@ -58,8 +48,11 @@ export default {
   },
   include: ["./scripts", "./test", "./typechain-types"],
   files: ["./hardhat.config.ts"],
+  typechain: {
+    outDir: "../shared/types",
+  },
   abiExporter: {
-    path: "../abi",
+    path: "../shared/abi",
     runOnCompile: true,
     clear: true,
     flat: true,
