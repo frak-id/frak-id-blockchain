b333c59f43696dc402b5c5cf32abe1a604e632a6
Quentin <quentin@nivelais.com>
2022-10-13 11:19:50 +0200

    Update mythril run

--- contracts/tools/mythril/mythril.sh
+++ contracts/tools/mythril/mythril.sh
@@ -6,34 +6,18 @@ if [ ! -d "contracts" ]; then
 	exit 1
 fi
 
-echo "<----- Checking SybelMath.sol ----->"
-analyse_contract contracts/utils/SybelMath.sol
-
-echo ""
-echo "<----- Checking SybelToken.sol ----->"
-analyse_contract contracts/tokens/SybelToken.sol
-
-echo ""
-echo "<----- Checking VestingWallets.sol ----->"
-analyse_contract contracts/wallets/VestingWallets.sol
-
-echo ""
-echo "<----- Checking SybelInternalTokens.sol ----->"
-analyse_contract contracts/tokens/SybelInternalTokens.sol
-
-echo ""
-echo "<----- Checking Minter.sol ----->"
-analyse_contract contracts/minter/Minter.sol
-
-echo ""
-echo "<----- Checking Rewarder.sol ----->"
-analyse_contract contracts/reward/Rewarder.sol
-
-echo ""
-echo "<----- Checking Referral.sol ----->"
-analyse_contract contracts/reward/Referral.sol
-
 # Run mythril analyse on the given contract
 function analyse_contract {
 	docker run --rm -v `pwd`:/src  --workdir=/src mythril/myth -v 4 analyze $1 --solc-json tools/mythril/remapping.json --max-depth 50
 }
+
+echo "<----- Checking SybelMath.sol ----->"
+analyse_contract contracts/utils/SybelMath.sol
+
+echo ""
+echo "<----- Checking MultiVestingWallets.sol ----->"
+analyse_contract contracts/wallets/MultiVestingWallets.sol
+
+echo ""
+echo "<----- Checking VestingWalletFactory.sol ----->"
+analyse_contract contracts/wallets/VestingWalletFactory.sol
