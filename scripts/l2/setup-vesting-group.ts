1a8f4c92c1e91e8360bffbcc0b2e66b9dedb8f2b
Quentin <quentin@nivelais.com>
2022-10-18 15:34:52 +0200

    Setup initial vesting group deployment

---
+++ scripts/l2/setup-vesting-group.ts
@@ -0,0 +1,71 @@
+import { BigNumber } from "ethers";
+
+import * as deployedAddresses from "../../addresses.json";
+import { VestingWalletFactory } from "../../types/contracts/wallets/VestingWalletFactory";
+import { findContract } from "../utils/deploy";
+
+const privateSaleGroupId = 1;
+const publicSaleGroupId = 2;
+const teamGroupId = 3;
+const techAndDevGroupId = 4;
+const marketGroupId = 5;
+const creatorGrantsGroupId = 6;
+const educationnalGroupId = 7;
+
+const monthAsSecond = 2628002.88;
+
+(async () => {
+  try {
+    console.log("Start to init our vesting groups");
+
+    // Get our contract
+    const vestingWalletFactory = await findContract<VestingWalletFactory>(
+      "VestingWalletFactory",
+      deployedAddresses.l2.vestingWalletFactory,
+    );
+
+    const decimals = BigNumber.from(10).pow(18);
+
+    // Create the groups
+    await vestingWalletFactory.addVestingGroup(
+      privateSaleGroupId,
+      decimals.mul(350_000_000),
+      0,
+      monthToSecond(24),
+      false,
+    );
+    await vestingWalletFactory.addVestingGroup(publicSaleGroupId, decimals.mul(70_000_000), 0, monthToSecond(3), false);
+    await vestingWalletFactory.addVestingGroup(teamGroupId, decimals.mul(250_000_000), 10, monthToSecond(36), true);
+    await vestingWalletFactory.addVestingGroup(
+      techAndDevGroupId,
+      decimals.mul(150_000_000),
+      0,
+      monthToSecond(36),
+      true,
+    );
+    await vestingWalletFactory.addVestingGroup(marketGroupId, decimals.mul(230_000_000), 10, monthToSecond(12), true);
+    await vestingWalletFactory.addVestingGroup(
+      creatorGrantsGroupId,
+      decimals.mul(60_000_000),
+      0,
+      monthToSecond(1),
+      false,
+    );
+    await vestingWalletFactory.addVestingGroup(
+      educationnalGroupId,
+      decimals.mul(60_000_000),
+      0,
+      monthToSecond(1),
+      false,
+    );
+
+    console.log("Finished to init our vesting groups");
+  } catch (e: any) {
+    console.log(e.message);
+  }
+})();
+
+function monthToSecond(month: number): BigNumber {
+  const asSecond = month * monthAsSecond;
+  return BigNumber.from(Math.round(asSecond));
+}
