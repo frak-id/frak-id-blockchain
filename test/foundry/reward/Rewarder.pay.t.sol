255d86ba650a7ea0f676093b31fb8d9a4be5f810
Quentin <quentin@sybel.co>
2022-12-26 03:54:06 +0100

    ✅ Splitting reward test, starting pay tests

---
+++ test/foundry/reward/Rewarder.pay.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GNU GPLv3
+pragma solidity 0.8.17;
+
+import { FrakMath } from "@frak/utils/FrakMath.sol";
+import "forge-std/Vm.sol";
+import { ProxyTester } from "@foundry-upgrades/ProxyTester.sol";
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { RewarderTestHelper } from "./RewarderTestHelper.sol";
+
+/// Testing the frak l2 token
+contract RewarderPayTest is RewarderTestHelper {
+    using FrakMath for uint256;
+
+    uint256 contentId;
+
+    function setUp() public {
+        _baseSetUp();
+
+        contentId = mintAContent();
+    }
+
+    /*
+     * ===== TEST : payUser(
+        address listener,
+        uint8 contentType,
+        uint256[] calldata contentIds,
+        uint16[] calldata listenCounts
+    )s =====
+     */
+    function testPayUser() public withFrkToken(rewarderAddr) prankExecAsDeployer {
+        uint16[] memory listenCounts = new uint16[](1);
+        listenCounts[0] = 10;
+        rewarder.payUser(address(1), 1, contentId.asSingletonArray(), listenCounts);
+    }
+
+    function testPayUserFuzz(uint16[] memory listenCounts) public withFrkToken(rewarderAddr) prankExecAsDeployer {
+        vm.assume(listenCounts.length < 21);
+
+        uint256[] memory contentIds = new uint256[](listenCounts.length);
+        for (uint256 i; i < contentIds.length;) {
+            unchecked {
+                contentIds[i] = contentId;
+                listenCounts[i] = 10;
+                i++;
+            }
+        }
+
+        rewarder.payUser(address(1), 1, contentIds, listenCounts);
+    }
+}
---
+++ test/foundry/reward/RewarderTestHelper.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: GNU GPLv3
+pragma solidity 0.8.17;
+
+import "@frak/tokens/FrakTokenL2.sol";
+import "@frak/tokens/FraktionTokens.sol";
+import "@frak/reward/pool/ContentPool.sol";
+import "@frak/reward/pool/ReferralPool.sol";
+import "@frak/reward/Rewarder.sol";
+import "@frak/tokens/FraktionTokens.sol";
+import "@frak/utils/FrakMath.sol";
+import "@frak/wallets/MultiVestingWallets.sol";
+import "forge-std/console.sol";
+import "forge-std/Vm.sol";
+import { ProxyTester } from "@foundry-upgrades/ProxyTester.sol";
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { FrkTokenTestHelper } from "../FrkTokenTestHelper.sol";
+
+/// Testing the frak l2 token
+contract RewarderTestHelper is FrkTokenTestHelper {
+    using FrakMath for address;
+    using FrakMath for uint256;
+
+    FraktionTokens fraktionTokens;
+    ContentPool contentPool;
+    ReferralPool referralPool;
+    address foundationAddr = address(13);
+
+    address rewarderAddr;
+    Rewarder rewarder;
+
+    address contentOwnerAddress = address(2);
+
+    function _baseSetUp() internal {
+        _setupFrkToken();
+
+        // Deploy fraktions token
+        bytes memory initData = abi.encodeCall(FraktionTokens.initialize, ("test_url"));
+        address fraktionProxyAddr = deployContract(address(new FraktionTokens()), initData);
+        fraktionTokens = FraktionTokens(fraktionProxyAddr);
+
+        // Deploy content pool
+        initData = abi.encodeCall(ContentPool.initialize, (fraktionProxyAddr));
+        address contentPoolProxyAddr = deployContract(address(new ContentPool()), initData);
+        contentPool = ContentPool(contentPoolProxyAddr);
+
+        // Deploy referral pool
+        initData = abi.encodeCall(ReferralPool.initialize, (address(frakToken)));
+        address referralProxyAddr = deployContract(address(new ReferralPool()), initData);
+        referralPool = ReferralPool(referralProxyAddr);
+
+        // Deploy fraktions token
+        initData = abi.encodeCall(
+            Rewarder.initialize,
+            (address(frakToken), fraktionProxyAddr, contentPoolProxyAddr, referralProxyAddr, foundationAddr)
+        );
+        rewarderAddr = deployContract(address(new Rewarder()), initData);
+        rewarder = Rewarder(rewarderAddr);
+
+        // Grant the right roles
+        _grantSetupRoles();
+    }
+
+    function _grantSetupRoles() private prankExecAsDeployer {
+        fraktionTokens.grantRole(FrakRoles.MINTER, rewarderAddr);
+        frakToken.grantRole(FrakRoles.MINTER, rewarderAddr);
+
+        contentPool.grantRole(FrakRoles.REWARDER, rewarderAddr);
+        referralPool.grantRole(FrakRoles.REWARDER, rewarderAddr);
+
+        contentPool.grantRole(FrakRoles.TOKEN_CONTRACT, address(fraktionTokens));
+
+        fraktionTokens.registerNewCallback(address(contentPool));
+    }
+
+
+    /*
+     * ===== UTILS=====
+     */
+
+    function mintAContent() public returns(uint256) {
+        prankDeployer();
+        return fraktionTokens.mintNewContent(contentOwnerAddress);
+    }
+}
