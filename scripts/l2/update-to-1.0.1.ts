4db937b8ff6f05b31cdc630d1fda377114b5a22f
Quentin <quentin@sybel.co>
2023-01-06 12:19:57 +0100

    🔨 Adding role managment to update script

--- scripts/l2/update-to-1.0.1.ts
+++ scripts/l2/update-to-1.0.1.ts
@@ -1,13 +1,11 @@
 import * as fs from "fs";
-import hre, { ethers, upgrades } from "hardhat";
+import hre from "hardhat";
 
 import * as deployedAddresses from "../../addresses.json";
 import { FrakToken, FrakTreasuryWallet } from "../../types";
 import { deployContract, findContract } from "../utils/deploy";
+import { adminRole, minterRole, pauserRole, upgraderRole } from "../utils/roles";
 import { updateContracts } from "../utils/updateContracts";
-
-import * as fs from "fs";
-import { minterRole } from "../utils/roles";
 
 (async () => {
   try {
@@ -18,6 +16,10 @@ import { minterRole } from "../utils/roles";
     // Find the right addresses for our current network
     const networkName = hre.hardhatArguments.network ?? "local";
     const addresses = networkName === "mumbai" ? deployedAddresses.mumbai : deployedAddresses.polygon;
+    const fireblocksAddr =
+      networkName === "mumbai"
+        ? "0x9f6f0915dA5452786A5A5Dc08fE5412a2981D746"
+        : "0x97Ce46bBC97aa20D22Cf98b4E37775A55ff70cAC";
 
     // Find our frak token contract
     const frakToken = await findContract<FrakToken>("FrakToken", addresses.frakToken);
@@ -27,12 +29,12 @@ import { minterRole } from "../utils/roles";
     console.log(`Treasury wallet was deployed to ${treasuryWallet.address}`);
 
     // Then verify our contract
-    console.log("Verifying treasury contract")
+    console.log("Verifying treasury contract");
     await hre.run("verify:verify", { address: treasuryWallet.address });
 
     // Grant the right role to the treasury wallet contract
     const grantRoleTx = await frakToken.grantRole(minterRole, treasuryWallet.address);
-    console.log(`Granting minter role on tx ${grantRoleTx.hash}`)
+    console.log(`Granting minter role on tx ${grantRoleTx.hash}`);
 
     // Update our deployed address object
     const addressesMap: Map<string, any> = new Map(Object.entries(deployedAddresses));
@@ -52,6 +54,21 @@ import { minterRole } from "../utils/roles";
       { name: "ReferralPool", address: addresses.referralPool },
     ];
     await updateContracts(nameToAddresses);
+
+    // Grant the admin roles on the treasury contract
+    console.log("Granting the role to the fireblocks wallet");
+    await treasuryWallet.grantRole(adminRole, fireblocksAddr);
+    await treasuryWallet.grantRole(pauserRole, fireblocksAddr);
+    await treasuryWallet.grantRole(upgraderRole, fireblocksAddr);
+    await treasuryWallet.grantRole(minterRole, fireblocksAddr);
+
+    // Grant the admin roles on the treasury contract
+    console.log("Revoking the role of the deployer wallet");
+    const selfAddress = await treasuryWallet.signer.getAddress();
+    await treasuryWallet.renounceRole(adminRole, selfAddress);
+    await treasuryWallet.renounceRole(pauserRole, selfAddress);
+    await treasuryWallet.renounceRole(upgraderRole, selfAddress);
+    await treasuryWallet.renounceRole(minterRole, selfAddress);
 
     console.log("Finished to update our contracts to v1.0.1");
   } catch (e: any) {
