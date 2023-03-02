d0f70fcc44f11eaf5537b9ec32dfbadbcd33a26b
Quentin <quentin@sybel.co>
2023-03-02 11:29:28 +0100

    🔨 Add update to 1.2.0 script

---
+++ scripts/l2/update-to-1.2.0.ts
@@ -0,0 +1,29 @@
+import hre from "hardhat";
+
+import * as deployedAddresses from "../../addresses.json";
+import { updateContracts } from "../utils/updateContracts";
+
+(async () => {
+  try {
+    console.log("Start to update our contracts to v1.2.0");
+
+    // Find the right addresses for our current network
+    const networkName = hre.hardhatArguments.network ?? "local";
+    const addresses = networkName === "mumbai" ? deployedAddresses.mumbai : deployedAddresses.polygon;
+
+    // Contract updated to assembly : FrkTokenL2 - FraktionTokens - Rewarder - ContentPool - FrakTreasuryWallet - Minter
+    const signer = await hre.ethers.getSigners();
+    console.log(signer);
+
+    // Update our contracts
+    const nameToAddresses = [
+      { name: "Minter", address: addresses.minter },
+      { name: "Rewarder", address: addresses.rewarder },
+    ];
+    await updateContracts(nameToAddresses);
+
+    console.log("Finished to update our contracts to v1.2.0");
+  } catch (e: any) {
+    console.log(e.message);
+  }
+})();
