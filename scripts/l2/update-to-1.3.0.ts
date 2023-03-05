f1b2700c5b0c65a957f83ceefc25857158b84af4
Quentin <quentin@sybel.co>
2023-03-05 17:08:13 +0100

    🚀 Create update script and deploy FrkToken to test and prod

---
+++ scripts/l2/update-to-1.3.0.ts
@@ -0,0 +1,26 @@
+import hre from "hardhat";
+
+import * as deployedAddresses from "../../addresses.json";
+import { updateContracts } from "../utils/updateContracts";
+
+(async () => {
+  try {
+    console.log("Start to update our contracts to v1.3.0");
+
+    // Find the right addresses for our current network
+    const networkName = hre.hardhatArguments.network ?? "local";
+    const addresses = networkName === "mumbai" ? deployedAddresses.mumbai : deployedAddresses.polygon;
+
+    // Update our contracts
+    // TODO : FrakToken pending on Polygon
+    const nameToAddresses = [
+      // { name: "Minter", address: addresses.minter },
+      { name: "FrakToken", address: addresses.frakToken },
+    ];
+    await updateContracts(nameToAddresses);
+
+    console.log("Finished to update our contracts to v1.3.0");
+  } catch (e: any) {
+    console.log(e.message);
+  }
+})();
