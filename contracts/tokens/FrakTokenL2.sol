97d162629ccd83e567ce0048e59334b4325717b3
KONFeature <quentin@nivelais.com>
2023-03-24 15:00:05 +0100

    ⚡️⚰️ Remove unused bridge code for now

--- contracts/tokens/FrakTokenL2.sol
+++ contracts/tokens/FrakTokenL2.sol
@@ -91,32 +91,6 @@ contract FrakToken is
     }
 
     /* -------------------------------------------------------------------------- */
-    /*                External write function's for Polygon bridge                */
-    /* -------------------------------------------------------------------------- */
-
-    /**
-     * @notice called when token is deposited on root chain
-     * @dev Should be callable only by ChildChainManager
-     * Should handle deposit by minting the required amount for user
-     * @param user user address for whom deposit is being done
-     * @param depositData abi encoded amount (required for polygon bridge)
-     */
-    function deposit(address user, bytes calldata depositData) external onlyRole(DEPOSITOR_ROLE) {
-        uint256 amount = abi.decode(depositData, (uint256));
-        if (totalSupply() + amount > _cap) revert CapExceed();
-        _mint(user, amount);
-    }
-
-    /**
-     * @notice called when user wants to withdraw tokens back to root chain
-     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
-     * @param amount amount of tokens to withdraw
-     */
-    function withdraw(uint256 amount) external {
-        _burn(_msgSender(), amount);
-    }
-
-    /* -------------------------------------------------------------------------- */
     /*                 Permit logic (greatly inspired by solmate)                 */
     /* -------------------------------------------------------------------------- */
 
