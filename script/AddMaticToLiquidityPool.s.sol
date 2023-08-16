368811fc1e8618b66b5f554fb464d181e94306c0
KONFeature <quentin@nivelais.com>
2023-08-16 11:28:53 +0200

    ⬆️ Switch to the new pool contract's

--- script/AddMaticToLiquidityPool.s.sol
+++ script/AddMaticToLiquidityPool.s.sol
@@ -4,15 +4,13 @@ pragma solidity 0.8.21;
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import {UpgradeScript} from "./utils/UpgradeScript.s.sol";
-import {MonoTokenPool} from "singleton-swapper/MonoTokenPool.sol";
-import {BaseEncoderLib} from "singleton-swapper/encoder/BaseEncoderLib.sol";
-import {MonoOpEncoderLib} from "singleton-swapper/encoder/MonoOpEncoderLib.sol";
+import {MonoPool} from "swap-pool/MonoPool.sol";
+import {EncoderLib} from "swap-pool/encoder/EncoderLib.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 
 contract AddMaticToLiquidityPool is UpgradeScript {
     using SafeTransferLib for address;
-    using BaseEncoderLib for bytes;
-    using MonoOpEncoderLib for bytes;
+    using EncoderLib for bytes;
 
     function run() external {
         // Get the current addresses
@@ -23,19 +21,8 @@ contract AddMaticToLiquidityPool is UpgradeScript {
             return;
         }
 
-        // Get the wmatic address
-        address wmatic;
-        if (block.chainid == 80001) {
-            wmatic = address(0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889);
-        } else if (block.chainid == 137) {
-            wmatic = address(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
-        } else {
-            console.log("Unsupported chain id: %s", block.chainid);
-            return;
-        }
-
         // Find the mono token pool
-        MonoTokenPool pool = MonoTokenPool(payable(addresses.swapPool));
+        MonoPool pool = MonoPool(payable(addresses.swapPool));
 
         // Address of the pool owner
         address poolOwner = address(0x7caF754C934710D7C73bc453654552BEcA38223F);
@@ -44,10 +31,8 @@ contract AddMaticToLiquidityPool is UpgradeScript {
         uint256 maticAmount = 1e18;
 
         // Build the command
-        (uint256 frkToDeposit, bytes memory addLiquidityCommand) =
-            _buildAddLiquidityCommand(addresses.frakToken, wmatic, poolOwner, maticAmount);
+        (uint256 frkToDeposit, bytes memory addLiquidityCommand) = _buildAddLiquidityCommand(poolOwner, maticAmount);
 
-        console.log("wMatic address: %s", wmatic);
         console.log("Frk to deposit: %s", frkToDeposit);
         console.log("Matic to deposit: %s", maticAmount);
 
@@ -56,7 +41,7 @@ contract AddMaticToLiquidityPool is UpgradeScript {
     }
 
     /// @dev Build the add liquidity command
-    function _buildAddLiquidityCommand(address frkToken, address wmatic, address owner, uint256 maticAmount)
+    function _buildAddLiquidityCommand(address owner, uint256 maticAmount)
         private
         pure
         returns (uint256 frkToDeposit, bytes memory program)
@@ -66,17 +51,17 @@ contract AddMaticToLiquidityPool is UpgradeScript {
 
         // Build the programm
         // forgefmt: disable-next-item
-        program = BaseEncoderLib
-            .init(4)
-            .appendAddLiquidity(wmatic, owner, frkToDeposit, maticAmount)
-            .appendReceive(wmatic, maticAmount, true)
-            .appendPullAll(frkToken)
+        program = EncoderLib
+            .init()
+            .appendAddLiquidity(owner, frkToDeposit, maticAmount)
+            .appendReceive(false, maticAmount, true)
+            .appendReceiveAll(true)
             .done();
     }
 
     /// @dev Execute the add liquidity command
     function _executeAddLiquidity(
-        MonoTokenPool pool,
+        MonoPool pool,
         bytes memory program,
         address frkToken,
         uint256 frkAmount,
--- script/DeploySwapPool.s.sol
+++ script/DeploySwapPool.s.sol
@@ -4,26 +4,44 @@ pragma solidity 0.8.21;
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import {UpgradeScript} from "./utils/UpgradeScript.s.sol";
-import {MonoTokenPool} from "singleton-swapper/MonoTokenPool.sol";
+import {MonoPool} from "swap-pool/MonoPool.sol";
 
 contract DeploySwapPool is UpgradeScript {
-    /// @dev The basis point for the pool to deploy (on 1e4)
-    uint256 private constant BPS = 0.05e4;
+    /// @dev The basis point for the pool to deploy
+    uint256 private constant BPS = 100;
 
     function run() external {
         // Get the current addresses
         UpgradeScript.ContractProxyAddresses memory addresses = _currentProxyAddresses();
 
+        // Get the wmatic address
+        address wmatic;
+        if (block.chainid == 80001) {
+            wmatic = address(0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889);
+        } else if (block.chainid == 137) {
+            wmatic = address(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
+        } else {
+            console.log("Unsupported chain id: %s", block.chainid);
+            return;
+        }
+
+        // Address of the fee receiver owner
+        address poolOwner = address(0x7caF754C934710D7C73bc453654552BEcA38223F);
+
         // Deploy the initial pool
-        MonoTokenPool pool = _deployInitialPool(addresses.frakToken);
+        MonoPool pool = _deployInitialPool(addresses.frakToken, wmatic, poolOwner);
 
         // Log the pool address
-        console.log("MonoTokenPool address: %s", address(pool));
+        console.log("MonoPool address: %s", address(pool));
     }
 
     /// @dev Fake some reward for a user
-    function _deployInitialPool(address frkToken) internal deployerBroadcast returns (MonoTokenPool monotokenPool) {
-        // Build the pool
-        monotokenPool = new MonoTokenPool(frkToken, BPS);
+    function _deployInitialPool(address frkToken, address wMatic, address feeReceiver)
+        internal
+        deployerBroadcast
+        returns (MonoPool monotokenPool)
+    {
+        // Build the pool, with 1% bps & 1% fee
+        monotokenPool = new MonoPool(frkToken, wMatic, 100, feeReceiver, 100);
     }
 }
